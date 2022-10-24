dcba26fd98641f49c341af030f7279617fa0199c
metr0xx <mishgan0702@gmail.com>
2022-10-24 23:14:29 +0300

    admin tools design

--- src/AdminPanel.jsx
+++ src/AdminPanel.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
+
 export default function AdminPanel() {
-    return (
-            <div>
-                aboba
-                </div>
-    )
-};
+  const [showCourseLectures, setShowCourseLectures] = useState(false);
+  const [addLecture, setAddLecture] = useState(false);
+  const [addCourse, setAddCourse] = useState(false);
+  const [lectures, setLectures] = useState([]);
+  const [courses, setCourses] = useState([]);
+
+  function showThisCourseLectures() {}
+
+  function addNewLecture() {}
+
+  function addNewCourse() {}
+
+  function changeCourseIndex() {}
+
+  useEffect(() => {
+    setCourses(["matan", "linal"]);
+    setLectures(["predeli", "martici"]);
+  });
+
+  return (
+    <body>
+      <div className="appBar">
+        <h3 className="title">Admin tools</h3>
+      </div>
+      {lectures.map((lec) => {
+        return <div></div>;
+      })}
+
+      {showCourseLectures && <div></div>}
+      <div className="course-list">
+        {/* {% for course in coursesList %} */}
+        <div className="row">
+          {/* <button onclick="showLectures()">{{course.title}}</button> */}
+        </div>
+      </div>
+
+      <div className="btnRow">
+        <button className="addLectureBtn" onclick="addCourse()">
+          <div className="addLectureTxt">Добавить курс</div>
+        </button>
+        <button className="addLectureBtn" onclick="addLecture()">
+          <div className="addLectureTxt">Добавить лекцию</div>
+        </button>
+      </div>
+    </body>
+  );
+}
