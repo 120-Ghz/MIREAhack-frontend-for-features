dc1ea82972de1dad65839abc5312b5fe97bb700b
metr0xx <mishgan0702@gmail.com>
2022-10-25 18:29:27 +0300

    lectures showing

--- src/AdminPanel.jsx
+++ src/AdminPanel.jsx
@@ -1,54 +1,74 @@
 import { useEffect, useState } from "react";
 
 export default function AdminPanel() {
-  const [showCourseLectures, setShowCourseLectures] = useState(false);
   const [addLecture, setAddLecture] = useState(false);
   const [addCourse, setAddCourse] = useState(false);
   const [lectures, setLectures] = useState([]);
   const [courses, setCourses] = useState([]);
-
-  function showThisCourseLectures() {}
-
-  function addNewLecture() {}
-
-  function addNewCourse() {}
-
-  function changeCourseIndex() {}
+  const [coursesIndexes, setCoursesIndexes] = useState([]);
 
   useEffect(() => {
-      fetch("http://127.0.0.1:5000/courses", {
-          method: 'GET'
-      })
-      .then(response => response.json())
-      .then(result => {
-          setLectures(result.lectures)
-          setCourses(result.courses)
-      })
-
+    fetch("http://127.0.0.1:5000/courses", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        setLectures(result.lectures);
+        setCourses(result.courses);
+        let indexes = [];
+        for (let i = 0; i < result.courses.length; i++) {
+          indexes[i] = 0;
+        }
+        setCoursesIndexes(indexes);
+      });
   }, []);
+  console.log(lectures, courses);
+  let changeCourseIndex = (ind) => {
+    let indexes = [...coursesIndexes];
+    indexes[ind] = !coursesIndexes[ind];
+    setCoursesIndexes(indexes);
+  };
 
   return (
-    <div>
+    <div className="page">
       <div className="appBar">
         <h3 className="title">Admin tools</h3>
       </div>
-      {lectures.map((lec) => {
-        return <div></div>;
-      })}
-
-      {showCourseLectures && <div></div>}
       <div className="course-list">
-        {/* {% for course in coursesList %} */}
-        <div className="row">
-          {/* <button onclick="showLectures()">{{course.title}}</button> */}
-        </div>
+        {courses.map((course, id) => {
+          return (
+            <div className="courLecRow">
+              <button onClick={() => changeCourseIndex(id)}>
+                <div className="courseBtn">{course["coursename"]}</div>
+              </button>
+              {coursesIndexes[id] ? (
+                lectures.map((lecture) => {
+                  return (
+                    <div className="column">
+                      {lectures[id]["courseId"] === course["courseId"]} ? (
+                      {lectures[id]["title"]}) : (<div />)
+                    </div>
+                  );
+                })
+              ) : (
+                <div />
+              )}
+            </div>
+          );
+        })}
       </div>
 
       <div className="btnRow">
-          <button className="addLectureBtn" onClick={addNewCourse()}>
+        <button
+          className="addLectureBtn"
+          onClick={() => setAddCourse(!addCourse)}
+        >
           <div className="addLectureTxt">Добавить курс</div>
         </button>
-          <button className="addLectureBtn" onClick={addNewLecture()}>
+        <button
+          className="addLectureBtn"
+          onClick={() => setAddLecture(!addLecture)}
+        >
           <div className="addLectureTxt">Добавить лекцию</div>
         </button>
       </div>
