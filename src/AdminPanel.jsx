c3aad5984c31e9264dbdd6f7b7841e7c3758def7
Konstantin <57593173+lalkalol1907@users.noreply.github.com>
2022-10-25 09:57:19 +0300

    add fetch to adminpanel

--- src/AdminPanel.jsx
+++ src/AdminPanel.jsx
@@ -16,12 +16,19 @@ export default function AdminPanel() {
   function changeCourseIndex() {}
 
   useEffect(() => {
-    setCourses(["matan", "linal"]);
-    setLectures(["predeli", "martici"]);
-  });
+      fetch("http://127.0.0.1:5000/courses", {
+          method: 'POST'
+      })
+      .then(response => response.json())
+      .then(result => {
+          setLectures(result.lectures)
+          setCourses(result.courses)
+      })
+
+  }, []);
 
   return (
-    <body>
+    <div>
       <div className="appBar">
         <h3 className="title">Admin tools</h3>
       </div>
@@ -38,13 +45,13 @@ export default function AdminPanel() {
       </div>
 
       <div className="btnRow">
-        <button className="addLectureBtn" onclick="addCourse()">
+          <button className="addLectureBtn" onClick={addNewCourse()}>
           <div className="addLectureTxt">Добавить курс</div>
         </button>
-        <button className="addLectureBtn" onclick="addLecture()">
+          <button className="addLectureBtn" onClick={addNewLecture()}>
           <div className="addLectureTxt">Добавить лекцию</div>
         </button>
       </div>
-    </body>
+    </div>
   );
 }
