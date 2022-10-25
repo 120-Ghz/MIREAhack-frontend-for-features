c7466de2b633ee1517b86150b3eb5a62becb02e0
Konstantin <57593173+lalkalol1907@users.noreply.github.com>
2022-10-25 13:35:02 +0300

    add login_fetch

--- src/AdminLogin.jsx
+++ src/AdminLogin.jsx
@@ -1,7 +1,45 @@
+import {useState} from "react";
+
 export default function AdminLogin() {
+    const [login, setLogin] = useState("")
+    const [password, setPassword] = useState("")
+
+    const passwordChanged = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const loginChanged = (event) => {
+        setLogin(event.target.value)
+    }
+
+    const formSubmit = (event) => {
+        fetch('http://127.0.0.1:5000/adminLogin', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "login": login,
+                "password": password,
+                "refresh_token": ""
+            })
+        })
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+        })
+        event.preventDefault()
+    }
+
     return (
-            <div>
-                abobaLogin
+        <form onSubmit={formSubmit}>
+            <div className="loginForm">
+                <label>Username : </label>
+                <input type="text" value={login} onChange={loginChanged} placeholder="Enter Username"/>
+                <label>Password : </label>
+                <input type="password" value={password} onChange={passwordChanged} placeholder="Enter Password"/>
+                <button type="submit">Login</button>
             </div>
-            )
+        </form>
+    )
 }
